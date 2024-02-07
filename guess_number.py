6bc9be83b414b14cebdecd819956b22419c6cddc
Vishal Chintakunta <vkchntkn@memphis.edu>
2024-02-07 16:57:47 -0600

    Added number of guesses

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,11 @@
 
 number = 10
+max_guesses = 5  
 print("I'm thinking of a number...")
 
-while True:
-    guess = input("What number am I thinking of? (Enter 'q' to quit): ")
+guesses_left = max_guesses  
+while guesses_left > 0:
+    guess = input(f"What number am I thinking of? (Enter 'q' to quit). You have {guesses_left} guesses left: ")
     if guess.lower() == 'q':
         print(f"The number was {number}.")
         break
@@ -13,6 +15,13 @@ while True:
             print("Congratulations! You guessed the right number.")
             break
         else:
-            print("Sorry! That's not it. Try again.")
+            guesses_left -= 1  
+            if guesses_left == 0:
+                print(f"Sorry, you're out of guesses. The number was {number}.")
+            else:
+                print("Sorry! That's not it. Try again.")
     except ValueError:
         print("Please enter a valid number or 'q' to quit.")
+
+if guesses_left == 0 and guess.lower() != 'q':  
+    print(f"Sorry, you're out of guesses. The number was {number}.")
